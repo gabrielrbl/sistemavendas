f71c3a51b2c79f5666c979a2cda0aa28b04c7357
netosep <clementesepulveda27@gmail.com>
2021-08-05 12:23:46 -0300

    Criando rota para buscar o produto por `id`

--- API/src/routes/produtos.js
+++ API/src/routes/produtos.js
@@ -1,12 +1,26 @@
 const { Router } = require("express");
-const { selectAll } = require("../controller/ProdutosController");
+const { findAll, findById } = require("../controller/ProdutosController");
 const db = require("../database/connection");
 
 const produtosRouter = Router();
 
 produtosRouter.get("/", (req, res, next) => {
 
-    selectAll(req, res, next);
+    return findAll(req, res, next);
+    
+});
+
+produtosRouter.get("/:id", (req, res, next) => {
+
+    const idProduto = req.params.id;
+
+    if (isNaN(idProduto)) {
+        return res.status(400).send({
+            message: "Parametro inválido"
+        })
+    }
+
+    return findById(idProduto, req, res, next);
     
 });
 
