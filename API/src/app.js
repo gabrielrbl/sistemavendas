a68342b8b4953a5f67d403bc8f8779df4baa5264
netosep <clementesepulveda27@gmail.com>
2021-08-05 12:26:07 -0300

    Adicionando retorno de `erro 404` para rota não encontrada

--- API/src/app.js
+++ API/src/app.js
@@ -9,4 +9,15 @@ app.use(express.json());
 app.use('/api', homeRouter);
 app.use('/api/produtos', produtosRouter);
 
+app.use((req, res, next) => {
+    const erro = new Error("Não encontrado");
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    return res.status(404).send({
+        error: error.message 
+    });
+});
+
 module.exports = { app };
