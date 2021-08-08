714bcf450567836ef3d48c13ea6109110c845167
netosep <clementesepulveda27@gmail.com>
2021-08-07 22:54:41 -0300

    Criando método `updateProduct` para a atualização de produto

--- API/src/controller/ProdutosController.js
+++ API/src/controller/ProdutosController.js
@@ -3,7 +3,7 @@ const db = require("../database/connection");
 const findAllProduct = async function(req, res, next) {
 
     await db.execute(`
-        SELECT * FROM PRODUTO
+        SELECT * FROM produto
     `)
     .then((results) => {
         res.status(200).send({
@@ -11,7 +11,7 @@ const findAllProduct = async function(req, res, next) {
             produtos: results
         });
     })
-    .catch(err => {
+    .catch((err) => {
         return res.status(500).send({
             error: err
         });
@@ -22,7 +22,7 @@ const findAllProduct = async function(req, res, next) {
 const findProductById = async function(idProduto, req, res, next) {
     
     await db.execute(`
-        SELECT * FROM PRODUTO WHERE CDPRODUTO = ?`, [idProduto]
+        SELECT * FROM produto WHERE cdproduto = ?`, [idProduto]
     )
     .then((result) => {
         if(result.length < 1){
@@ -34,7 +34,78 @@ const findProductById = async function(idProduto, req, res, next) {
             produto: result[0]
         });
     })
-    .catch(err => {
+    .catch((err) => {
+        return res.status(500).send({
+            error: err
+        });
+    });
+
+}
+
+/* const insertProduct = async function(req, res, next) {
+
+    const produto = {
+        descricao : req.body.descricao.toUpperCase(),
+        marca     : req.body.marca.toUpperCase()
+    }
+
+    await db.execute(`
+        INSERT INTO produto (cdproduto, descricao, marca)
+        VALUES (?, ?, ?)
+    `, [
+        1, produto.descricao, produto.marca
+    ])
+    .then((result) => {
+        return res.status(201).send({
+            result: result
+            //message: "Produto inserido com sucesso"
+        });
+    })
+    .catch((err) => {
+        return res.status(500).send({
+            error: err
+        });
+    });
+
+}  */
+
+const updateProduct = async function(idProduto, req, res, next) {
+
+    const produto = {
+        id        : idProduto,
+        descricao : req.body.descricao.toUpperCase(),
+        marca     : req.body.marca.toUpperCase()
+    }
+
+    await db.execute(`
+        SELECT marca FROM produto WHERE cdproduto = ?`, [produto.id]
+    )
+    .then(async (result) => {
+        if(result.length < 1){
+            return res.status(404).send({
+                message: "Produto não existente"
+            });
+        }
+        await db.execute(`
+            UPDATE produto
+            SET descricao   = ?,
+                marca       = ?
+            WHERE cdproduto = ?
+        `, [
+            produto.descricao, produto.marca, produto.id
+        ])
+        .then(() => {
+            return res.status(200).send({
+                message: "Produto atualizado com sucesso"
+            });
+        })
+        .catch((err) => {
+            return res.status(500).send({
+                error: err
+            });
+        });
+    })
+    .catch((err) => {
         return res.status(500).send({
             error: err
         });
@@ -43,6 +114,8 @@ const findProductById = async function(idProduto, req, res, next) {
 }
 
 module.exports = {
-    findAll,
-    findById
+    findAllProduct,
+    findProductById,
+    //insertProduct,
+    updateProduct
 }
